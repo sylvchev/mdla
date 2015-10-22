efa5e8592ca61167a726af81cb5cacedc4473764
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2015-10-22 02:41:36 +0200

    correctly display axes

--- experiment_dictionary_recovering.py
+++ experiment_dictionary_recovering.py
@@ -7,7 +7,7 @@ from mdla import multivariate_sparse_encode
 from dict_metrics import hausdorff, emd, detectionRate, betaDist
 from numpy.linalg import norm
 from numpy import array, arange, zeros, min, max
-from numpy.random import rand, randn, permutation, randint
+from numpy.random import rand, randn, permutation, randint, RandomState
 import pickle
 from os.path import exists
 
@@ -31,8 +31,8 @@ def _generate_testbed(kernel_init_len, n_nonzero_coefs, n_kernels,
     signals = list()
     decomposition = list()
     for i in range(n_samples):
-        s = np.zeros(shape=(n_features, n_dims))
-        d = np.zeros(shape=(n_nonzero_coefs, 3))
+        s = zeros(shape=(n_features, n_dims))
+        d = zeros(shape=(n_nonzero_coefs, 3))
         rk = permutation(range(n_kernels))
         for j in range(n_nonzero_coefs):
             k_idx = rk[j]
@@ -49,7 +49,7 @@ def _generate_testbed(kernel_init_len, n_nonzero_coefs, n_kernels,
             pn = norm(noise, 'fro')
             alpha = ps / (pn*10**(snr/20.))
         signals.append(s+alpha*noise)
-    signals = np.array(signals)
+    signals = array(signals)
 
     return dico, signals, decomposition
 
@@ -63,30 +63,31 @@ def plot_recov(wc, wfs, hc, hfs, bd, dr99, dr97, n_iter, figname):
     for i, s in enumerate(snr):
         # plotting data from detection rate
         detection = fig.add_subplot(3, 4, i*4+1)
-        det99 = detection.boxplot(dr99[i,:,:])
+        det99 = detection.boxplot(dr99[i,:,:]/100.)
         plt.setp(det99['medians'], color='green')
         plt.setp(det99['caps'], color='green')
         plt.setp(det99['boxes'], color='green')
         plt.setp(det99['fliers'], color='green')
         plt.setp(det99['whiskers'], color='green')
         medianlt99 = [median.get_ydata()[0]
-                      for n,median in enumerate(det99['medians'])]
-        axlt99 = detection.plot(np.arange(1,n+2), medianlt99,
+                      for n, median in enumerate(det99['medians'])]
+        axlt99 = detection.plot(arange(1, n_iter+1), medianlt99,
                                 linewidth=1, color='green',
                                 label=r'$c_\operatorname{99}$')
-        det97 = detection.boxplot(dr97[i,:,:])
+        det97 = detection.boxplot(dr97[i,:,:]/100.)
         plt.setp(det97['medians'], color='magenta')
         plt.setp(det97['caps'], color='magenta')
         plt.setp(det97['boxes'], color='magenta')
         plt.setp(det97['fliers'], color='magenta')
         plt.setp(det97['whiskers'], color='magenta')
         medianlt97 = [median.get_ydata()[0]
-                      for n,median in enumerate(det97['medians'])]
-        axlt97 = detection.plot(np.arange(1,n+2), medianlt97,
+                      for n, median in enumerate(det97['medians'])]
+        axlt97 = detection.plot(arange(1, n_iter+1), medianlt97,
                                 linewidth=1, color='magenta',
                                 label=r'$c_\operatorname{97}$')
-        detection.axis([0, n, 0, 100])
-        detection.set_xticks(np.arange(0,n+1,10))
+        detection.axis([0, n_iter, 0, 1])
+        detection.set_xticks(arange(0, n_iter+1, 10))
+        detection.set_xticklabels([])
         detection.set_title(r'SNR '+s)
         detection.legend(loc='lower right')
     
@@ -100,7 +101,7 @@ def plot_recov(wc, wfs, hc, hfs, bd, dr99, dr97, n_iter, figname):
         plt.setp(hausch['whiskers'], color='cyan')
         medianhc = [median.get_ydata()[0]
                     for n,median in enumerate(hausch['medians'])]
-        axhc = methaus.plot(np.arange(1,n+2), medianhc, linewidth=1,
+        axhc = methaus.plot(arange(1, n_iter+1), medianhc, linewidth=1,
                             label=r'$1-d_H^c$', color='cyan')
         hausfs = methaus.boxplot(1-hfs[i,:,:]) 
         plt.setp(hausfs['medians'], color='yellow')
@@ -109,11 +110,13 @@ def plot_recov(wc, wfs, hc, hfs, bd, dr99, dr97, n_iter, figname):
         plt.setp(hausfs['fliers'], color='yellow')
         plt.setp(hausfs['whiskers'], color='yellow')
         medianhfs = [median.get_ydata()[0]
-                     for n,median in enumerate(hausfs['medians'])]
-        axhfs = methaus.plot(np.arange(1,n+2), medianhfs, linewidth=1,
+                     for n, median in enumerate(hausfs['medians'])]
+        axhfs = methaus.plot(arange(1, n_iter+1), medianhfs, linewidth=1,
                              label=r'$1-d_H^{fs}$', color='yellow')
-        methaus.axis([0, n, 0, 1])
-        methaus.set_xticks(np.arange(0,n+1,10))
+        methaus.axis([0, n_iter, 0, 1])
+        methaus.set_xticks(arange(0, n_iter+1, 10))
+        methaus.set_xticklabels([])
+        methaus.set_yticklabels([])
         methaus.legend(loc='lower right')
         methaus.set_title(r'SNR '+s)
 
@@ -126,8 +129,8 @@ def plot_recov(wc, wfs, hc, hfs, bd, dr99, dr97, n_iter, figname):
         plt.setp(wassch['fliers'], color='red')
         plt.setp(wassch['whiskers'], color='red')
         medianwc = [median.get_ydata()[0]
-                    for n,median in enumerate(wassch['medians'])]
-        axwc = metwass.plot(np.arange(1,n+2), medianwc, linewidth=1,
+                    for n, median in enumerate(wassch['medians'])]
+        axwc = metwass.plot(arange(1, n_iter+1), medianwc, linewidth=1,
                             label=r'$1-d_W^c$', color='red')
         wassfs = metwass.boxplot(1-wfs[i,:,:]) 
         plt.setp(wassfs['medians'], color='blue')
@@ -136,11 +139,13 @@ def plot_recov(wc, wfs, hc, hfs, bd, dr99, dr97, n_iter, figname):
         plt.setp(wassfs['fliers'], color='blue')
         plt.setp(wassfs['whiskers'], color='blue')
         medianwfs = [median.get_ydata()[0]
-                     for n,median in enumerate(wassfs['medians'])]
-        axwfs = metwass.plot(np.arange(1,n+2), medianwfs, linewidth=1,
+                     for n, median in enumerate(wassfs['medians'])]
+        axwfs = metwass.plot(arange(1, n_iter+1), medianwfs, linewidth=1,
                              label=r'$1-d_W^{fs}$', color='blue')
-        metwass.axis([0, n, 0, 1])
-        metwass.set_xticks(np.arange(0, n+1, 10))
+        metwass.axis([0, n_iter, 0, 1])
+        metwass.set_xticks(arange(0, n_iter+1, 10))
+        metwass.set_xticklabels([])
+        metwass.set_yticklabels([])
         metwass.legend(loc='lower right')
         metwass.set_title(r'SNR '+s)
     
@@ -153,14 +158,24 @@ def plot_recov(wc, wfs, hc, hfs, bd, dr99, dr97, n_iter, figname):
         plt.setp(betad['fliers'], color='black')
         plt.setp(betad['whiskers'], color='black')
         medianbd = [median.get_ydata()[0]
-                    for n,median in enumerate(betad['medians'])]
-        axbd = metbeta.plot(np.arange(1,n+2), medianbd, linewidth=1,
+                    for n, median in enumerate(betad['medians'])]
+        axbd = metbeta.plot(arange(1, n_iter+1), medianbd, linewidth=1,
                             label=r'$1-d_\beta$', color='black')
-        metbeta.axis([0, n, 0, 1])
-        metbeta.set_xticks(np.arange(0, n+1, 10))
+        metbeta.axis([0, n_iter, 0, 1])
+        metbeta.set_xticks(arange(0, n_iter+1, 10))
+        metbeta.set_xticklabels([])
+        metbeta.set_yticklabels([])
         metbeta.legend(loc='lower right')
         metbeta.set_title(r'SNR '+s)
-        
+
+    detection.set_xticks(arange(0, n_iter+1, 10))
+    detection.set_xticklabels(arange(0, n_iter+1, 10))
+    methaus.set_xticks(arange(0, n_iter+1, 10))
+    methaus.set_xticklabels(arange(0, n_iter+1, 10))
+    metwass.set_xticks(arange(0, n_iter+1, 10))
+    metwass.set_xticklabels(arange(0, n_iter+1, 10))
+    metbeta.set_xticks(arange(0, n_iter+1, 10))
+    metbeta.set_xticklabels(arange(0, n_iter+1, 10))
     plt.tight_layout(.5)
     plt.savefig(figname+".png")
 
@@ -180,7 +195,7 @@ def callback_recovery(loc):
     d.dr97.append(detectionRate(loc['dictionary'],
                                 d.generating_dict, 0.97))
 
-rng_global = np.random.RandomState(1)
+rng_global = RandomState(1)
 n_samples, n_dims, n_kernels = 1500, 1, 50
 n_features = kernel_init_len = 20
 n_nonzero_coefs, learning_rate = 3, 1.5
