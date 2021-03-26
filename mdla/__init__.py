5bac8d17f0b4da3c0d4003b9a61ed26c47933502
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-03-26 21:08:04 +0100

    adding poetry and modifying module struture

---
+++ mdla/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .mdla import (
+    MiniBatchMultivariateDictLearning,
+    MultivariateDictLearning,
+    SparseMultivariateCoder,
+    multivariate_sparse_encode,
+    reconstruct_from_code,
+)
